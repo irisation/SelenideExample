db7c8c477f5b634c3fb4ecc051b46444b5bcc101
irisation <irisationmeister@gmail.com>
2015-02-16 21:10:27 +0300

    initial commit - final

--- src/test/java/HowToStartTest.java
+++ src/test/java/HowToStartTest.java
@@ -1,11 +1,16 @@
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.testng.BrowserPerTest;
+import com.codeborne.selenide.testng.ScreenShooter;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
@@ -15,19 +20,30 @@ import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.Condition.disappear;
 import static com.codeborne.selenide.Condition.text;
 
-
+@Listeners({BrowserPerTest.class, ScreenShooter.class})
+//указываем когда и как мы создаём браузер -- для каждого теста(класса);
+// задаём создание скриншотов
 public class HowToStartTest {
 
     @BeforeMethod
     public void setup() {
-        Configuration.browser = "chrome";
-        Configuration.baseUrl = "http://the-internet.herokuapp.com/";
+//        Configuration.browser = "chrome"; default is firefox -- не оставлять в коде!
+        // mvn -Dbrowser=chrome
+        Configuration.baseUrl = "http://the-internet.herokuapp.com";
+        Configuration.timeout = 10000;
+//        Configuration.remote = "http://localhost:4444/wd/hub";
+
+        //Если необходимо работать с драйвером, его можно сконфигурировать заранее(например, подключить плагин и тд),
+        // а потом передать в селенид
+//        WebDriver driver = new FirefoxDriver(new DesiredCapabilities());
+//        WebDriverRunner.setWebDriver(driver);
     }
 
     @Test
     public void startTest() {
         open("/dynamic_controls");
-        $(By.linkText("Dynamic Controls")).click();
+//        open("http://the-internet.herokuapp.com");
+//        $(By.linkText("Dynamic Controls")).click();
         $(By.id("btn")).click();
         $(By.id("checkbox")).should(disappear);
     }
